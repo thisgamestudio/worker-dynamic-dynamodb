fedfa6c3fd0b34912733252504a8cffaa6532481
Bruno Fuster <bruno@thisgamestudio.com>
2016-04-03 20:29:28 -0700

    Build and beanstalk

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+SHORT_HASH=$(git rev-parse --short HEAD)
+FILE=build/dynamic-dynamo-${SHORT_HASH}.zip
+echo Writing $FILE ...
+zip -r $FILE .ebextensions .aws bin .dockerignore etc Docker* 
