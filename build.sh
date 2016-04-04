5f639e6ec35327d7f44b4756baadc895d53341ae
Bruno Fuster <bruno@thisgamestudio.com>
2016-04-03 21:46:54 -0700

    update build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 SHORT_HASH=$(git rev-parse --short HEAD)
 FILE=build/dynamic-dynamo-${SHORT_HASH}.zip
 echo Writing $FILE ...
-zip -r $FILE .ebextensions .aws bin .dockerignore etc Docker* 
+zip -r $FILE .ebextensions .aws bin .dockerignore etc Docker* boot
